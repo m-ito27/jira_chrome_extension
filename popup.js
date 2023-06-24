5f41d3c39d1bb2d001d29fb0d8f1f25f7342df63
Ito Masaki <msk.nfl.teabow12@gmail.com>
2023-06-24 15:57:14 +0900

    タスク一覧をコピーできる機能を追加

--- popup.js
+++ popup.js
@@ -11,16 +11,28 @@ window.onload = function() {
             console.error(chrome.runtime.lastError);
           } else {
             // Use clipboard directly in the popup page.
-            navigator.clipboard.writeText(result[0].result).then(() => {
-              console.log("Copied successfully");
-            }).catch((err) => {
-              console.error("Error copying text: ", err);
-            });
+            navigator.clipboard.writeText(result[0].result)
           }
         }
       );
     });
   });
+
+  document.getElementById('all-task-copy-btn').addEventListener('click', function() {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      chrome.scripting.executeScript({
+        target: {tabId: tabs[0].id},
+        func: getTasksText,
+      }, (results) => {
+        if (chrome.runtime.lastError) {
+          console.error(chrome.runtime.lastError);
+        } else {
+          navigator.clipboard.writeText(results[0].result);
+        }
+      });
+    });
+  })
+
 }
 
 function getUrlAndH1() {
@@ -29,4 +41,22 @@ function getUrlAndH1() {
 
   let taskTitle = document.querySelector('h1[data-test-id="issue.views.issue-base.foundation.summary.heading"]').innerText
   return `${jiraID} ${taskTitle}`;
+}
+
+function getTasksText() {
+  let tasksElement = document.getElementById('your-work-dropdown-tabs-0-tab');
+  let copyText = ''
+  reversedTaskList = Array.from(tasksElement.querySelectorAll("[data-ds--menu--heading-item='true']")).reverse()
+  reversedTaskList.forEach((element) => {
+    jiraStatus = element.innerText
+    copyText = copyText.concat(`[${jiraStatus}]\n`)
+    parent = element.parentElement
+    parent.querySelectorAll("[data-item-title='true']").forEach((eachTask) => {
+      taskTitle = eachTask.innerText
+      jiraID = eachTask.parentElement.querySelector("[data-item-description='true']").childNodes[0].textContent
+      // console.log(`${jiraID} ${taskTitle}`)
+      copyText = copyText.concat(`${jiraID} ${taskTitle}\n`)
+    })
+  })
+  return copyText
 }
